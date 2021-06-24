4b2f765f597471bb9ad2cfb4668b8acb90f9f6bc
uchicago-langlab <86124521+uchicago-langlab@users.noreply.github.com>
2021-06-24 13:10:48 -0500

    Add files via upload

---
+++ js_includes/MultiSlider.js
@@ -0,0 +1,35 @@
+/* This software is licensed under a BSD license; see the LICENSE file for details. */
+
+define_ibex_controller({
+name: "MultiSlider",
+   
+    
+jqueryWidget: {
+
+    _init: function () {
+                          
+        var opts = {
+            // this.options.transfer = null; // Remove 'click to continue message'
+            options:     this.options,
+            triggers:    [7],
+            children:    ["Form", {html: {include: this.options.include}},
+                          "StaticSentence2",{s2: this.options.s2},
+                          "Scale_NoButton", {html1: this.options.html1},
+                          "Scale_NoButton2", {html2: this.options.html2},
+                          "Scale_NoButton3", {html3: this.options.html3},
+                          "Scale_NoButton4", {html4: this.options.html4},
+                          "Scale_New", {html5: this.options.html5},
+                          "Vorm", {html6: this.options.html6}] /*,
+            manipulators: [
+                [0, function(div) { div.css('font-size', "larger"); return div; }]
+            ]*/
+        };
+
+        this.element.VBox(opts);
+    }
+},
+
+properties: {
+    obligatory: ["include","s2"]
+}
+});
