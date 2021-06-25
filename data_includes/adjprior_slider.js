53d64e64fbcb58ac8a60ff81ff4389275ec7093e
uchicago-langlab <86124521+uchicago-langlab@users.noreply.github.com>
2021-06-25 15:29:02 -0500

    Add files via upload

--- data_includes/adjprior_slider.js
+++ data_includes/adjprior_slider.js
@@ -1,5 +1,5 @@
 
-var shuffleSequence = seq("consent", "intro", "practice1_intro", "practice1", "practice2_intro", "practice2", rshuffle(startsWith("item-")), "questionnaire", "exit");
+var shuffleSequence = seq("consent", "intro", "practice1_intro", "practice1", "practice2_intro", "practice2", "end_practice", rshuffle(startsWith("item-")), "questionnaire", "exit");
 
 var showProgressBar = false;
 var pageTitle = "Mechanical Turk Experiment";
